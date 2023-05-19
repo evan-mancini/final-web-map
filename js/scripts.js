686d3bfd59a8ed5507daf9299fd47d0bf4fc93d7
evan-mancini <elm8214@nyu.edu>
2023-05-19 00:14:02 -0400

    progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,538 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXZhbm1hbmNpbmkiLCJhIjoiY2xnNXE5d2NlMDJxazNxcDhyc2g1eGo2eCJ9.9FsLDvuMxvT0C4cWbIsQAw';
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v11',
+    style: 'mapbox://styles/mapbox/dark-v11',
     center: [-73.91770592106197, 40.70528610017079],
-    zoom: 10.5
+    zoom: 10.75
 })
 
-// adding 3D buildings
+map.on('style.load', function () {
 
-map.on('style.load', () => {
-    const layers = map.getStyle().layers;
-    const labelLayerId = layers.find(
-        (layer) => layer.type === 'symbol' && layer.layout['text-field']
-    ).id; map.addLayer(
-        {
-            'id': 'add-3d-buildings',
-            'source': 'composite',
-            'source-layer': 'building',
-            'filter': ['==', 'extrude', 'true'],
-            'type': 'fill-extrusion',
-            'minzoom': 15,
-            'paint': {
-                'fill-extrusion-color': '#aaa',
-                'fill-extrusion-height': [
-                    'interpolate',
-                    ['linear'],
-                    ['zoom'],
-                    15,
-                    0,
-                    15.05,
-                    ['get', 'height']
-                ],
-                'fill-extrusion-base': [
-                    'interpolate',
-                    ['linear'],
-                    ['zoom'],
-                    15,
-                    0,
-                    15.05,
-                    ['get', 'min_height']
-                ],
-                'fill-extrusion-opacity': 0.6
-            }
-        },
-        labelLayerId
-    );
-});
-
-map.on('style.load', (
     map.addSource('subway_routes', {
         'type': 'geojson',
-        'data': '.data/routes_subway.geojson'
+        'data': 'data/routes_subway.geojson'
     }), map.addLayer({
         'id': 'fill-subway_routes',
         'type': 'line',
         'source': 'subway_routes',
         'layout': {},
         'paint': {
-            'line-color': '#0080ff',
-            'line-width': 0.5
+            'line-color': '#546bff',
+            'line-width': 3
         }
-    })))
+    }), map.on('click', 'fill-subway_routes', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_id)
+            .addTo(map);
+    });
+})
+
+$('#fly-to-bayside').on('click', function () {
+    map.flyTo({
+        center: [-73.76430045862422, 40.771418928210714],
+        zoom: 12.5
+    }),
+    map.addSource('localbus_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_localbus.geojson'
+    }), map.addLayer({
+        'id': 'fill-localbus_routes',
+        'type': 'line',
+        'source': 'localbus_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#b6d9b8',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-localbus_routes', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_id)
+            .addTo(map);
+    });
+    map.addSource('expressbus_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_expressbus.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_expressbus',
+        'type': 'line',
+        'source': 'expressbus_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#6b095c',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-routes_expressbus', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_id)
+            .addTo(map);
+    });
+    map.addSource('lirr_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_lirr.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_lirr',
+        'type': 'line',
+        'source': 'lirr_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#ff0004',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-routes_lirr', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_long)
+            .addTo(map)
+    })
+    map.addSource('mnr_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_metronorth.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_metronorth',
+        'type': 'line',
+        'source': 'mnr_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#e1ff00',
+            'line-width': 1
+        }
+    }), map.on('click', 'fill-routes_metronorth', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_long)
+            .addTo(map)
+    }),
+        map.addSource('local-bus-stops', {
+            'type': 'geojson',
+            'data': 'data/stops_localbus.geojson'
+        }), map.addLayer({
+            'id': 'fill-local-bus-stops',
+            'type': 'circle',
+            'source': 'local-bus-stops',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222'
+            }
+        }), map.on('click', 'fill-local-bus-stops', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+
+        }), map.addSource('express-bus-stops', {
+            'type': 'geojson',
+            'data': 'data/stops_expressbus.geojson'
+        }), map.addLayer({
+            'id': 'fill-express-bus-stops',
+            'type': 'circle',
+            'source': 'express-bus-stops',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222'
+            }
+        }), map.on('click', 'fill-express-bus-stops', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+        })
+
+        map.addSource('LIRR_stations', {
+            'type': 'geojson',
+            'data': 'data/stops_lirr.geojson'
+        }), map.addLayer({
+            'id': 'fill-LIRR_stations',
+            'type': 'circle',
+            'source': 'LIRR_stations',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222',
+                'circle-opacity': .5
+            }
+        }), map.on('click', 'fill-LIRR_stations', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+
+    }), map.addSource('MNR_stations', {
+        'type': 'geojson',
+        'data': 'data/stops_metronorth.geojson'
+    }), map.addLayer({
+        'id': 'fill-MNR_stations',
+        'type': 'circle',
+        'source': 'MNR_stations',
+        'layout': {},
+        'paint': {
+            'circle-radius': 4,
+            'circle-color': '#96b9f2'
+        }
+    }), map.on('click', 'fill-LIRR_stations', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.stop_name)
+            .addTo(map)
+
+
+}) , map.addSource('bayside_iso', {
+    'type': 'geojson',
+    'data': 'data/bayside_isochrone.geojson'
+}), map.addLayer({
+    'id': 'fill-bayside',
+    'type': 'fill',
+    'source': 'bayside_iso',
+    'layout': {},
+    paint: {
+        'fill-color': '#FF0000',
+        'fill-opacity': 0.5
+      }
+})
+})
+
+$('#fly-to-marine').on('click', function () {
+    map.flyTo({
+        center: [-73.9335657922829, 40.61153404163023],
+        zoom: 12.5
+    }),
+    map.addSource('localbus_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_localbus.geojson'
+    }), map.addLayer({
+        'id': 'fill-localbus_routes',
+        'type': 'line',
+        'source': 'localbus_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#b6d9b8',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-localbus_routes', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_id)
+            .addTo(map);
+    });
+    map.addSource('expressbus_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_expressbus.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_expressbus',
+        'type': 'line',
+        'source': 'expressbus_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#6b095c',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-routes_expressbus', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_id)
+            .addTo(map);
+    });
+    map.addSource('lirr_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_lirr.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_lirr',
+        'type': 'line',
+        'source': 'lirr_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#ff0004',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-routes_lirr', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_long)
+            .addTo(map)
+    })
+
+    map.addSource('mnr_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_metronorth.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_metronorth',
+        'type': 'line',
+        'source': 'mnr_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#e1ff00',
+            'line-width': 1
+        }
+    }), map.on('click', 'fill-routes_metronorth', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_long)
+            .addTo(map)
+    }),
+        map.addSource('local-bus-stops', {
+            'type': 'geojson',
+            'data': 'data/stops_localbus.geojson'
+        }), map.addLayer({
+            'id': 'fill-local-bus-stops',
+            'type': 'circle',
+            'source': 'local-bus-stops',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222'
+            }
+        }), map.on('click', 'fill-local-bus-stops', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+
+        }), map.addSource('express-bus-stops', {
+            'type': 'geojson',
+            'data': 'data/stops_expressbus.geojson'
+        }), map.addLayer({
+            'id': 'fill-express-bus-stops',
+            'type': 'circle',
+            'source': 'express-bus-stops',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222'
+            }
+        }), map.on('click', 'fill-express-bus-stops', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+        })
+
+        map.addSource('LIRR_stations', {
+            'type': 'geojson',
+            'data': 'data/stops_lirr.geojson'
+        }), map.addLayer({
+            'id': 'fill-LIRR_stations',
+            'type': 'circle',
+            'source': 'LIRR_stations',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222',
+                'circle-opacity': .5
+            }
+        }), map.on('click', 'fill-LIRR_stations', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+
+    }), map.addSource('MNR_stations', {
+        'type': 'geojson',
+        'data': 'data/stops_metronorth.geojson'
+    }), map.addLayer({
+        'id': 'fill-MNR_stations',
+        'type': 'circle',
+        'source': 'MNR_stations',
+        'layout': {},
+        'paint': {
+            'circle-radius': 4,
+            'circle-color': '#96b9f2'
+        }
+    }), map.on('click', 'fill-LIRR_stations', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.stop_name)
+            .addTo(map)
+
+
+}) ,  map.addSource('marinepark_iso', {
+    'type': 'geojson',
+    'data': 'data/marinepark_isochrone.geojson'
+}), map.addLayer({
+    'id': 'fill-marine',
+    'type': 'fill',
+    'source': 'marinepark_iso',
+    'layout': {},
+    paint: {
+        'fill-color': '#4287f5',
+        'fill-opacity': .5
+      }
+})
+})
+
+$('#fly-to-maspeth').on('click', function () {
+    map.flyTo({
+        center: [-73.90655308825586, 40.72923636097061],
+        zoom: 12.5
+    }),
+    map.addSource('localbus_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_localbus.geojson'
+    }), map.addLayer({
+        'id': 'fill-localbus_routes',
+        'type': 'line',
+        'source': 'localbus_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#b6d9b8',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-localbus_routes', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_id)
+            .addTo(map);
+    });
+    map.addSource('expressbus_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_expressbus.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_expressbus',
+        'type': 'line',
+        'source': 'expressbus_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#6b095c',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-routes_expressbus', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_id)
+            .addTo(map);
+    });
+    map.addSource('lirr_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_lirr.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_lirr',
+        'type': 'line',
+        'source': 'lirr_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#ff0004',
+            'line-width': 3
+        }
+    }), map.on('click', 'fill-routes_lirr', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_long)
+            .addTo(map)
+    })
+
+    map.addSource('mnr_routes', {
+        'type': 'geojson',
+        'data': 'data/routes_metronorth.geojson'
+    }), map.addLayer({
+        'id': 'fill-routes_metronorth',
+        'type': 'line',
+        'source': 'mnr_routes',
+        'layout': {},
+        'paint': {
+            'line-color': '#e1ff00',
+            'line-width': 1
+        }
+    }), map.on('click', 'fill-routes_metronorth', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.route_long)
+            .addTo(map)
+    }),
+        map.addSource('local-bus-stops', {
+            'type': 'geojson',
+            'data': 'data/stops_localbus.geojson'
+        }), map.addLayer({
+            'id': 'fill-local-bus-stops',
+            'type': 'circle',
+            'source': 'local-bus-stops',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222'
+            }
+        }), map.on('click', 'fill-local-bus-stops', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+
+        }), map.addSource('express-bus-stops', {
+            'type': 'geojson',
+            'data': 'data/stops_expressbus.geojson'
+        }), map.addLayer({
+            'id': 'fill-express-bus-stops',
+            'type': 'circle',
+            'source': 'express-bus-stops',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222'
+            }
+        }), map.on('click', 'fill-express-bus-stops', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+        })
+
+        map.addSource('LIRR_stations', {
+            'type': 'geojson',
+            'data': 'data/stops_lirr.geojson'
+        }), map.addLayer({
+            'id': 'fill-LIRR_stations',
+            'type': 'circle',
+            'source': 'LIRR_stations',
+            'layout': {},
+            'paint': {
+                'circle-radius': 1,
+                'circle-color': '#B42222',
+                'circle-opacity': .5
+            }
+        }), map.on('click', 'fill-LIRR_stations', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.stop_name)
+                .addTo(map)
+
+    }), map.addSource('MNR_stations', {
+        'type': 'geojson',
+        'data': 'data/stops_metronorth.geojson'
+    }), map.addLayer({
+        'id': 'fill-MNR_stations',
+        'type': 'circle',
+        'source': 'MNR_stations',
+        'layout': {},
+        'paint': {
+            'circle-radius': 4,
+            'circle-color': '#96b9f2'
+        }
+    }), map.on('click', 'fill-LIRR_stations', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.stop_name)
+            .addTo(map)
+
+}) , map.addSource('maspeth_iso', {
+    'type': 'geojson',
+    'data': 'data/maspeth_isochrone.geojson'
+}), map.addLayer({
+    'id': 'fill-maspeth',
+    'type': 'fill',
+    'source': 'maspeth_iso',
+    'layout': {},
+    paint: {
+        'fill-color': '#c6db07',
+        'fill-opacity': .5
+      }
+}),
+
+$('#fly-to-city').on('click', function () {
+    map.flyTo({
+        center: [-73.91770592106197, 40.70528610017079],
+        zoom: 10.5
+    }),
+    map.removeLayer('fill-maspeth')
+})})
